e8bf8346d9e158e078b2f67cd0a9a034d937edb1
RubenGallien <ruben-gallienpro@outlook.fr>
2026-01-04 02:31:49 +0100

    feat: add leva with hemisphere and directional lights + rotate planet with useFrame

--- src/Experience/Lights/Lights.jsx
+++ src/Experience/Lights/Lights.jsx
@@ -1,11 +1,55 @@
 import { useHelper } from "@react-three/drei"
-import { useRef } from "react"
+import { button, useControls } from "leva"
+import { useRef, useState } from "react"
 import * as THREE from 'three'
 
 export default function Light(){
 
-	const directionnalLightRef = useRef()
+	const [lightHelper, setLightHelper] = useState(true)
+	const directionalLightRef = useRef()
 
-	useHelper(directionnalLightRef, THREE.DirectionalLightHelper, 1)
-	return <directionalLight ref={directionnalLightRef} intensity={3} position={[-3, 1, 2]}/>
+	const { directionalLightIntensity, dirLightColor } = useControls(
+		'DirectionalLight',
+		{
+			directionalLightIntensity: {
+				value: 3,
+				min: 0,
+				max: 10,
+				step: 0.01
+			},
+			dirLightColor: '#ffffff',
+			hideHlper: button(() => {
+				setLightHelper(!lightHelper);
+			})
+		}
+	)
+
+	const { hemispherLightIntensity, groundColor, skyColor } = useControls(
+		'HemisphereLight',
+		{
+			hemispherLightIntensity: {
+				value: 0.3,
+				min: 0,
+				max: 5,
+				step: 0.001
+			},
+			skyColor: '#8899ff',
+			groundColor: '#080820'
+		}
+	)
+
+	useHelper(lightHelper ? directionalLightRef : null, THREE.DirectionalLightHelper, 1)
+
+	return	<>
+		<directionalLight
+			color={dirLightColor}
+			ref={directionalLightRef}
+			intensity={directionalLightIntensity}
+			position={[-3, 2, 1]}
+			/>
+		<hemisphereLight
+			color={skyColor}
+			groundColor={groundColor}
+			intensity={hemispherLightIntensity} />
+	</>
 }
--- src/Experience/Model/SeinePeniche.jsx
+++ src/Experience/Model/SeinePeniche.jsx
@@ -1,9 +1,17 @@
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 
 export default function SeinePeniche() {
 
+
 	const seineModel = useGLTF('seineConflansPlanet.glb')
+
+	useFrame((state, delta) => {
+
+		if (seineModel.scene)
+			seineModel.scene.rotation.y += delta * 0.2
+	})
 
 	return <>
 		<primitive object={seineModel.scene} scale={1} />
